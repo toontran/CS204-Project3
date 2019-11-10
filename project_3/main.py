ef647822761fe27471bd8839a8c1b672dadc8e43
Arsh Noor Amin <ana002@brki167-lnx-9.bucknell.edu>
2019-11-10 16:23:04 -0500

    AVL skeleton

--- project_3/main.py
+++ project_3/main.py
@@ -1,4 +1,15 @@
+from bridges.bridges import *
+from bridges.avl_tree_element import *
 import sys
-import bridges
-import math
 
+class AVL_Tree:
+	def __init__(self, title, descr):
+		#add a title and description to the tree
+		bridges.set_title(title)
+		bridges.set_description(descr)
+		
+		root = None
+		
+	def create_node(self, idx, item):
+		""" create a new tree node """
+		pass
